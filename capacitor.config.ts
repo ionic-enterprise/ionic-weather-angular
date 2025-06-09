3dbd0705c3038d99241b2fbcad8d7ffecc4b3ec6
Ken Sodemann <kenneth.sodemann@outsystems.com>
2025-06-09 16:42:56 -0500

    upgrade to cap 7

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,6 +4,14 @@ const config: CapacitorConfig = {
   appId: 'io.ionic.ionicweatherng',
   appName: 'ionic-weather-angular',
   webDir: 'www',
+  android: {
+    adjustMarginsForEdgeToEdge: 'auto',
+  },
+  plugins: {
+    StatusBar: {
+      overlaysWebView: false,
+    },
+  },
 };
 
 export default config;
